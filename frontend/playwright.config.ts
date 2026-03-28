14ed82dd6eddbdeada4ccda7ff549f3af518d5f6
Algefmarc Anthony Almocera <algefalmocera@gmail.com>
2026-03-28 20:26:07 +0800

    feat: Finalize stores

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -2,13 +2,14 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests/e2e',
+  timeout: 60000,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    baseURL: 'http://127.0.0.1:5173',
+    baseURL: 'http://localhost:5173',
     trace: 'on-first-retry',
   },
   projects: [
